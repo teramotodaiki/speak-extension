49ca3172bc0eb71973ffec058f2b596de9e71a4d
teramotodaiki <i101323@gmail.com>
2020-01-29 17:36:16 +0900

    format with prettier

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
-var recognition, div, languageSelected, isStopButtonClicked = false;
+var recognition,
+  div,
+  languageSelected,
+  isStopButtonClicked = false;
 
 const init = () => {
   div = document.createElement('div');
@@ -9,7 +12,7 @@ const init = () => {
   recognition = new webkitSpeechRecognition();
   recognition.continuous = false;
   recognition.interimResults = true;
-  recognition.lang = languageSelected || "en-US";
+  recognition.lang = languageSelected || 'en-US';
 
   recognition.onresult = event => {
     let last = event.results.length - 1;
@@ -18,7 +21,7 @@ const init = () => {
     let final_transcript = '';
 
     for (var i = event.resultIndex; i < event.results.length; ++i) {
-        // Verify if the recognized text is the last with the isFinal property
+      // Verify if the recognized text is the last with the isFinal property
       if (event.results[i].isFinal) {
         final_transcript += event.results[i][0].transcript;
       } else {
@@ -28,28 +31,29 @@ const init = () => {
 
     div.textContent = interim_transcript;
     document.body.appendChild(div);
-  }
+  };
 
   recognition.onerror = event => {
-    console.log("error", event.error)
-    if(event.error === 'not-allowed'){
-      const errorMessage = "AudioCapture permission has been blocked because of a Feature Policy applied to the current document. See https://goo.gl/EuHzyv for more details.";
-      chrome.runtime.sendMessage({error: errorMessage})
+    console.log('error', event.error);
+    if (event.error === 'not-allowed') {
+      const errorMessage =
+        'AudioCapture permission has been blocked because of a Feature Policy applied to the current document. See https://goo.gl/EuHzyv for more details.';
+      chrome.runtime.sendMessage({ error: errorMessage });
       isStopButtonClicked = true;
       recognition.stop();
     }
-  }
+  };
 
-  recognition.onspeechstart = event => console.log("speech started");
+  recognition.onspeechstart = event => console.log('speech started');
   recognition.onspeechend = event => stopTracking();
   recognition.onend = function(event) {
     if (isStopButtonClicked) {
-      stopTracking()
+      stopTracking();
     } else {
-      startTracking()
+      startTracking();
     }
-  }
-}
+  };
+};
 
 const startTracking = () => recognition.start();
 
@@ -65,26 +69,26 @@ const setDivStyle = div => {
   div.style.width = '50%';
   div.style.transform = 'translate(50%)';
   div.style.border = '2px solid white';
-  div.style.borderRadius = "5px";
-  div.style.zIndex= "10000";
-  div.style.fontFamily = "Arial";
-}
+  div.style.borderRadius = '5px';
+  div.style.zIndex = '10000';
+  div.style.fontFamily = 'Arial';
+};
 
 const stopTracking = () => {
   recognition.stop();
-  if(document.body.contains(div)){
+  if (document.body.contains(div)) {
     document.body.removeChild(div);
   }
-}
+};
 
 init();
 
-chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-  if(request.data === "start"){
-    isStopButtonClicked = false
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.data === 'start') {
+    isStopButtonClicked = false;
     startTracking();
-  } else if(request.data === "stop") {
-    isStopButtonClicked = true
+  } else if (request.data === 'stop') {
+    isStopButtonClicked = true;
     stopTracking();
   } else {
     languageSelected = request.data;
